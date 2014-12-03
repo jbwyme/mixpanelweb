56933687a261a2ffd07640fe3a14b869745078be
jbwyme <wymetyme@gmail.com>
2014-12-03 21:23:58 +0000

    fixing paths

--- proxy.php
+++ proxy.php
@@ -11,8 +11,7 @@ echo $txt;
 ?>
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
-<script type="text/javascript" src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"></script>
-<script type="text/javascript" src="http://localhost/mixpanel/visual_editor.js"></script>
+<script type="text/javascript" src="visual_editor.js"></script>
 <script type="text/javascript">
 new MixpanelVisualWeb("1ef7e30d2a58d27f4b90c42e31d6d7ad")
 </script>
